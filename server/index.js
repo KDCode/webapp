f6ff330467eb929e2c65afda5999728321e8350d
KDCode <68739152+KDCode@users.noreply.github.com>
2021-06-13 20:32:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 
-const CONNECTION_URL ='mongodb+srv://kdsdatabase:kdsdatabase123@cluster0.zmcic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL ='mongodb+srv://kdsdatabase:*************@cluster0.zmcic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT  = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
